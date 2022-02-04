13ef5060543d042aca28e0b0c2ff03cdfa997f53
codeddave <drizzzydave@gmail.com>
2022-02-04 16:49:08 +0100

    updated assets, navbar styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-import Logo from "../../assets/icons/book.svg";
+import Logo from "../../assets/icons/books.svg";
 import CartIcon from "../../assets/icons/cart.svg";
+
+import Brand from "../../assets/icons/brand-full.svg";
 
 import "./Navbar.scss";
 const Navbar = () => {
   return (
     <nav>
       <div>
-        <div className="logo-wrapper">
+        {/*  <div className="logo-wrapper">
           <img src={Logo} alt="logo" />
         </div>
 
         <div className="logo-text-wrapper">
           <p>Quidax Books</p>
           <p>A flimsy book company</p>
-        </div>
+        </div> */}
+
+        <img src={Brand} alt="" srcset="" />
       </div>
       <div></div>
 
-      <div>
+      <div className="logo-end">
         <div className="book-wrapper">
           <img src={Logo} alt="logo" />
         </div>
