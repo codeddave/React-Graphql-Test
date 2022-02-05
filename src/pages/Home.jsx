6c1f1bf14629c5b720ead29c84f131d8a0dd25cc
codeddave <drizzzydave@gmail.com>
2022-02-05 23:45:27 +0100

    updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import AllBooks from "../components/AllBooks/AllBooks";
+import BookCarousel from "../components/BookCarousel/BookCarousel";
 
 const Home = () => {
   return (
     <div>
+      <BookCarousel />
       <AllBooks />
     </div>
   );
