8ff64d51c48ed9d36aa151b4b1f191acb470d62f
codeddave <drizzzydave@gmail.com>
2022-02-05 23:44:07 +0100

    updates to BookDetail page

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { getBook } from "../../api/queries";
 import ImageOne from "../../assets/images/big-magic-cover_euc3rh.jpg";
-
+import "./BookDetail.scss";
 const BookDetail = ({ book }) => {
   const { id } = useParams();
 
@@ -18,16 +18,28 @@ const BookDetail = ({ book }) => {
   console.log(data);
   return (
     <div className="book-detail-container">
-      <section>
-        <img src={data.book.image_url} alt="book" />
+      <section
+        style={{ border: "1px solid black" }}
+        className="book-detail-info"
+      >
+        <div className="book-detail-image-container">
+          <img src={data.book.image_url} alt="book" />
+        </div>
 
-        <p>23 copies available</p>
-        <p>$29.99</p>
+        <p className="book-detail-copies">
+          {data.book.available_copies} copies available
+        </p>
+        <p className="book-detail-price">${data.book.price}</p>
 
         <button>Add to Cart</button>
       </section>
-      <section>
-        <h3>Big Magic: Creative Living Beyond Fear</h3>
+      <section
+        style={{ border: "1px solid black" }}
+        className="book-detail-description"
+      >
+        <h3>
+          {data.book.title}: {data.book.subtitle}
+        </h3>
         <p>Elizabeth GIlbert</p>
       </section>
     </div>
