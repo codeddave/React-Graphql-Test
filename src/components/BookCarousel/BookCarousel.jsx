b3028b5957f78df33a69f7d411eeaa73b78d87a5
codeddave <drizzzydave@gmail.com>
2022-02-05 21:44:17 +0100

    updates to Carousel

--- src/components/BookCarousel/BookCarousel.jsx
+++ src/components/BookCarousel/BookCarousel.jsx
@@ -6,38 +6,78 @@ import ImageTwo from "../../assets/images/built-to-last-cover_cb92oa.jpg";
 import ImageThree from "../../assets/images/the-lean-startup-cover_dmwrjj.jpg";
 import { Carousel } from "react-responsive-carousel";
 import "./BookCarousel.scss";
+import Slider from "infinite-react-carousel";
+
 const BookCarousel = () => {
+  const settings = {
+    slidesPerRow: 8,
+    dots: true,
+    autoplay: true,
+  };
   return (
-    <section className="carousel-container">
-      <Carousel infiniteLoop autoPlay showThumbs={false}>
-        <div style={{ display: "flex" }} className="set">
-          <img src={ImageThree} alt="book22" className="carousel-image" />
-          <img src={ImageThree} alt="book22" className="carousel-image" />
-          <img src={ImageThree} alt="book22" className="carousel-image" />
-          <img src={ImageThree} alt="book22" className="carousel-image" />
-          <img src={ImageThree} alt="book22" className="carousel-image" />
-          <img src={ImageThree} alt="book22" className="carousel-image" />
-          <img src={ImageThree} alt="book22" className="carousel-image" />
-          <img src={ImageThree} alt="book22" className="carousel-image" />
-          <img src={ImageThree} alt="book22" className="carousel-image" />
-          <img src={ImageThree} alt="book22" className="carousel-image" />
-          <img src={ImageThree} alt="book22" className="carousel-image" />
-          <img src={ImageThree} alt="book22" className="carousel-image" />
-        </div>
-        <div style={{ display: "flex" }} className="set">
-          <img src={ImageThree} alt="book22" className="carousel-image" />
-          <img src={ImageOne} alt="book22" className="carousel-image" />
-          <img src={ImageOne} alt="book22" className="carousel-image" />
-          <img src={ImageOne} alt="book22" className="carousel-image" />
-          <img src={ImageOne} alt="book22" className="carousel-image" />
-          <img src={ImageOne} alt="book22" className="carousel-image" />
-          <img src={ImageOne} alt="book22" className="carousel-image" />
-          <img src={ImageOne} alt="book22" className="carousel-image" />
-          <img src={ImageOne} alt="book22" className="carousel-image" />
-          <img src={ImageOne} alt="book22" className="carousel-image" />
-          <img src={ImageOne} alt="book22" className="carousel-image" />
-        </div>
-      </Carousel>
+    <section>
+      <div className="carousel-title">
+        <h2 className="all-books-title">Featured Books</h2>
+        <div className="border-line" />
+      </div>
+
+      <section className="carousel-container">
+        <div className="left-arrow"></div>
+
+        <Slider
+          {...settings}
+          classNam="full"
+          nextArrow={
+            <button
+              type="button"
+              className="arrow"
+              style={{ color: "green", display: "block" }}
+            >
+              d.vljbjlb qlshkDCVqkhjvckhjqVCKHJQvkqwhvwkhv
+            </button>
+          }
+          prevArrow={
+            <button
+              type="button"
+              className="arrow"
+              style={{ color: "green", display: "block" }}
+            >
+              d.vljbjlb qlshkDCVqkhjvckhjqVCKHJQvkqwhvwkhv
+            </button>
+          }
+        >
+          <div>
+            <img src={ImageThree} alt="book22" className="carousel-image" />
+          </div>
+          <div>
+            <img src={ImageThree} alt="book22" className="carousel-image" />
+          </div>
+          <div>
+            <img src={ImageThree} alt="book22" className="carousel-image" />
+          </div>
+          <div>
+            <img src={ImageThree} alt="book22" className="carousel-image" />
+          </div>
+          <div>
+            <img src={ImageThree} alt="book22" className="carousel-image" />
+          </div>
+          <div>
+            <img src={ImageThree} alt="book22" className="carousel-image" />
+          </div>
+          <div>
+            <img src={ImageThree} alt="book22" className="carousel-image" />
+          </div>
+          <div>
+            <img src={ImageThree} alt="book22" className="carousel-image" />
+          </div>
+          <div>
+            <img src={ImageThree} alt="book22" className="carousel-image" />
+          </div>
+          <div>
+            <img src={ImageThree} alt="book22" className="carousel-image" />
+          </div>
+        </Slider>
+      </section>
     </section>
   );
 };
