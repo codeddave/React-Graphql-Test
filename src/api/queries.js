e33b9e70d35e8a79d29b685facc8bd2a809a85f1
codeddave <drizzzydave@gmail.com>
2022-02-07 05:09:52 +0100

    updated queries

--- src/api/queries.js
+++ src/api/queries.js
@@ -3,8 +3,30 @@ import { gql } from "@apollo/client";
 export const getAllBooks = gql`
   {
     books {
+      id
       title
       image_url
+      rating
+      likes
+      price
+      available_copies
+      full_description
+      featured
+      number_of_purchases
+      authors {
+        name
+        id
+      }
+      tags {
+        name
+        id
+      }
+      publisher
+      genres {
+        name
+        id
+      }
+      subtitle
     }
   }
 `;
@@ -12,6 +34,7 @@ export const getAllBooks = gql`
 export const getBook = gql`
   query getBook($id: ID!) {
     book(id: $id) {
+      id
       title
       image_url
       rating
