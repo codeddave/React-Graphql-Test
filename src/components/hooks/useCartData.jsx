d6d638d6a0ad0ab0c8dc6c30c56b48617ba5f91e
codeddave <drizzzydave@gmail.com>
2022-02-07 12:33:07 +0100

    updated hook

--- src/components/hooks/useCartData.jsx
+++ src/components/hooks/useCartData.jsx
@@ -5,10 +5,12 @@ export function useCartData() {
     JSON.parse(localStorage.getItem("cartData"))
   );
   const cartLength = JSON.parse(localStorage.getItem("cartData"))?.cart.length;
+  const cartTotal = JSON.parse(localStorage.getItem("cartData"))?.cartTotal;
+
   useEffect(() => {
     setCartData(JSON.parse(localStorage.getItem("cartData")));
-  }, [cartLength]);
-
+  }, [cartLength, cartTotal]);
+  // console.log(JSON.parse(localStorage.getItem("cartData")));
   return {
     cartData: cartData ? cartData : null,
     setCartData,
