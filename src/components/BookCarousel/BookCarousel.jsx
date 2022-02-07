ffe2ec17eeb97dc60224f41ba70dc0746c5efc5a
codeddave <drizzzydave@gmail.com>
2022-02-07 06:34:36 +0100

    finished arrow functionality

--- src/components/BookCarousel/BookCarousel.jsx
+++ src/components/BookCarousel/BookCarousel.jsx
@@ -4,15 +4,36 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import ImageOne from "../../assets/images/big-magic-cover_euc3rh.jpg";
 import ImageTwo from "../../assets/images/built-to-last-cover_cb92oa.jpg";
 import ImageThree from "../../assets/images/the-lean-startup-cover_dmwrjj.jpg";
+import PrevIcon from "../../assets/icons/back.svg";
+import NextIcon from "../../assets/icons/next.svg";
+
 import { Carousel } from "react-responsive-carousel";
 import "./BookCarousel.scss";
-import Slider from "infinite-react-carousel";
-
+import Slider from "react-slick";
+const PrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div onClick={onClick} className="prev-arrow arrow">
+      <img src={PrevIcon} alt="previous" />
+    </div>
+  );
+};
+const NextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div onClick={onClick} className="next-arrow arrow">
+      <img src={NextIcon} alt="next" />
+    </div>
+  );
+};
 const BookCarousel = () => {
   const settings = {
     slidesPerRow: 8,
     dots: true,
-    autoplay: true,
+    infinite: true,
+    speed: 1000,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
   };
   return (
     <section>
@@ -22,30 +43,7 @@ const BookCarousel = () => {
       </div>
 
       <section className="carousel-container">
-        <div className="left-arrow"></div>
-
-        <Slider
-          {...settings}
-          classNam="full"
-          nextArrow={
-            <button
-              type="button"
-              className="arrow"
-              style={{ color: "green", display: "block" }}
-            >
-              d.vljbjlb qlshkDCVqkhjvckhjqVCKHJQvkqwhvwkhv
-            </button>
-          }
-          prevArrow={
-            <button
-              type="button"
-              className="arrow"
-              style={{ color: "green", display: "block" }}
-            >
-              d.vljbjlb qlshkDCVqkhjvckhjqVCKHJQvkqwhvwkhv
-            </button>
-          }
-        >
+        <Slider {...settings} classNam="full">
           <div>
             <img src={ImageThree} alt="book22" className="carousel-image" />
           </div>
