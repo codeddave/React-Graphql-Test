15e7e41493ae5ecb409f31c2b450221b37c69c55
codeddave <drizzzydave@gmail.com>
2022-02-07 14:57:11 +0100

    created serch contextA

---
+++ src/context/search.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const SearchContext = React.createContext(undefined);
+
+export const SearchProvider = ({ children }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  return (
+    <SearchContext.Provider value={{ searchQuery, setSearchQuery }}>
+      {children}
+    </SearchContext.Provider>
+  );
+};
