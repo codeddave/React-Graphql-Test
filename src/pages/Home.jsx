620fc158790ac78a3c36077620ad37d9ca9a4b39
codeddave <drizzzydave@gmail.com>
2022-02-08 09:10:04 +0100

    updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import AllBooks from "../components/AllBooks/AllBooks";
 import BookCarousel from "../components/BookCarousel/BookCarousel";
+import SearchResults from "../components/SearchResults/SearchResults";
+import { SearchContext } from "../context/search";
 
 const Home = () => {
+  const { searchQuery, setSearchQuery } = useContext(SearchContext);
+
   return (
-    <div>
-      <BookCarousel />
-      <AllBooks />
+    <div style={{ width: "100%" }}>
+      {searchQuery ? null : <BookCarousel />}
+
+      {searchQuery ? <SearchResults /> : <AllBooks />}
     </div>
   );
 };
