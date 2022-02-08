e1429fae21301a13828775b94654f2e119d5f4f2
codeddave <drizzzydave@gmail.com>
2022-02-08 09:07:28 +0100

    updated query to get featured books

--- src/api/queries.js
+++ src/api/queries.js
@@ -31,6 +31,68 @@ export const getAllBooks = gql`
   }
 `;
 
+export const getBooksByFilter = gql`
+  query getBooksByFilter($filter: String) {
+    books(where: { title_contains: $filter }) {
+      id
+      title
+      image_url
+      rating
+      likes
+      price
+      available_copies
+      full_description
+      featured
+      number_of_purchases
+      authors {
+        name
+        id
+      }
+      tags {
+        name
+        id
+      }
+      publisher
+      genres {
+        name
+        id
+      }
+      subtitle
+    }
+  }
+`;
+
+export const getFeaturedBooks = gql`
+  query getFeaturedBooks {
+    books(where: { featured: true }) {
+      id
+      title
+      image_url
+      rating
+      likes
+      price
+      available_copies
+      full_description
+      featured
+      number_of_purchases
+      authors {
+        name
+        id
+      }
+      tags {
+        name
+        id
+      }
+      publisher
+      genres {
+        name
+        id
+      }
+      subtitle
+    }
+  }
+`;
+
 export const getBook = gql`
   query getBook($id: ID!) {
     book(id: $id) {
