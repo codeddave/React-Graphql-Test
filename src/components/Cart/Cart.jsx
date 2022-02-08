be13fefeb1faea0b49b66f67b3369745b0bf85bb
codeddave <drizzzydave@gmail.com>
2022-02-08 12:00:26 +0100

    updates

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import CartItem from "../CartItem/CartItem";
 import "./Cart.scss";
 import CartIcon from "../../assets/icons/cart.svg";
+import CartIconWhite from "../../assets/icons/cartt.svg";
 
 import BackIcon from "../../assets/icons/arrow.svg";
 import { ProductContext } from "../../context/cart";
@@ -29,6 +30,21 @@ const Cart = () => {
         cartData?.cart.map((cartItem) => (
           <CartItem key={cartItem.id} book={cartItem} />
         ))}
+
+      <section className="cart-total">
+        <div>
+          <div className="cart-total-wrapper">
+            <span>SubTotal</span>
+
+            <p>${cartData.cartTotal}</p>
+          </div>
+          <button className="cart-proceed-btn">
+            {" "}
+            <img className="book-detail-cart-icon" src={CartIconWhite} alt="" />
+            <span> Proceed to Checkout</span>
+          </button>
+        </div>
+      </section>
     </div>
   );
 };
